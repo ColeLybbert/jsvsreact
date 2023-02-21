53123b67567da877c093f9f032b2994ac733b3ab
Cole <cole.lybbert65@gmail.com>
2023-02-20 17:19:57 -0800

    react site done

--- react-file/src/TodoList.js
+++ react-file/src/TodoList.js
@@ -1,11 +1,38 @@
+import { useState } from "react"
+
 export default function TodoList() {
+    // Hooks
+    const [todoList, updateTodoList] = useState([]);
+    const [userInput, updateUserInput] = useState('');
+
+    const onChange = (event) => {updateUserInput(event.target.value)}
     return (
-        <div class="container">
-            <div id="newtask">
-                <input type="text" placeholder="Add Tasks"></input>
-                <button id="push">Add</button>
+        <div id="newtask">
+            <input type="text" placeholder="Add Tasks" onChange={onChange}/>
+            <button id="push" onClick={() => {
+                if (todoList.includes(userInput)) {
+                    alert("Get Smarter");
+                    return;
+                } else {
+                    updateTodoList([
+                        ...todoList, userInput 
+                    ])
+                }
+            }}>Add</button>
+            <div className="container">
+                {todoList.map(todo => {
+                    return (
+                        <div key={todo}>
+                            <p>{todo}</p>
+                            <button onClick={() => {
+                                updateTodoList([
+                                    ...todoList.filter(todoToRemove => { return todoToRemove !== todo })
+                                ])
+                            }}>X</button>
+                        </div>
+                    )
+                })}
             </div>
-            <div id="tasks"></div>
         </div>
     )
 }
